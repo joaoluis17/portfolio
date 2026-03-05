fa650e0465186f2c6a459b7ecb0c8faa22b154ba
joaoluis17 <jluispprado@hotmail.com>
2026-03-05 20:41:00 -0300

    arrumando o certificates para ts

---
+++ src/data/certificates.ts
@@ -0,0 +1,50 @@
+export default [
+  {
+    title: 'Agile Explorer - IBM',
+    description: 'Curso com certificação de 8 horas em Desenvolvimento Ágil pela IBM.',
+    fullDescription: 'Nesse plano de aprendizagem, aprendi do básico ao avançado sobre desenvolvimento ágil, com a metodologia da IBM.',
+    link: 'https://www.credly.com/badges/135198a1-a6cb-4e1c-b758-0d0478d2da1a/linked_in_profile',
+  },
+  {
+    title: 'Liderança e Gestão De Equipes',
+    description: 'Curso com certificação de 30 horas em Resolução de Problemas pelo ENAP.',
+    fullDescription: 'Curso focado em gestão de equipes e resolução de problemas.',
+    link: 'https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:576fd501-64ed-47a9-a5bc-fdb849e419a4',
+  },
+  {
+    title: 'OKR Aplicado a Transformacao Digital',
+    description: 'Curso com certificação de 25 horas pelo ENAP.',
+    fullDescription: 'Curso voltado para a aplicação de OKR em projetos de transformação digital.',
+    link: 'https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:5751b4ff-730c-43d2-8d11-64e361d76bc9',
+  },
+  {
+    title: 'Lean Manufacturing - Nível 1',
+    description: 'Curso com certificação de 2 horas em Lean Manufacturing pela RL&Associados.',
+    fullDescription: 'Nesse curso introdutório de nível 1, aprendi a base do Lean Manufacturing.',
+    link: 'https://acrobat.adobe.com/id/urn:aaid:sc:va6c2:e6339682-0579-4e5d-8abb-2f00b81f207b',
+  },
+  {
+    title: 'Desenvolvendo Times de Alta Performance',
+    description: 'Curso com certificação de 30 horas pelo ENAP.',
+    fullDescription: 'Aprendizado sobre performance de times, OKR e Kanban.',
+    link: 'https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:5751b4ff-730c-43d2-8d11-64e361d76bc9',
+  },
+  {
+    title: 'Formação White Belt Six Sigma',
+    description: 'Curso com certificação de 2 horas em Lean White Belt Six Sigma pela RL&Associados.',
+    fullDescription: 'Certificação White Belt onde aprendi a base do Six Sigma.',
+    link: 'https://acrobat.adobe.com/id/urn:aaid:sc:va6c2:71e8cd41-c921-4dde-ba61-0c1e65bb5eee',
+  },
+  {
+    title: 'Organização de Equipes Ágeis',
+    description: 'Curso com certificação de 6 horas pela Alura.',
+    fullDescription: 'Organização de equipes com métodos ágeis.',
+    link: 'https://cursos.alura.com.br/certificate/ce5bb624-df2f-4b12-bc12-60adebd00a0b?lang',
+  },
+  {
+    title: 'Imersão Dev 9ª Edição',
+    description: 'Workshop online com certificação de 4 horas pela Alura.',
+    fullDescription: 'Imersão focada em JavaScript e lógica de programação.',
+    link: 'https://cursos.alura.com.br/immersion/certificate/e7db9c3d-2271-4dd2-8d22-60390338c577?lang',
+  }
+];
