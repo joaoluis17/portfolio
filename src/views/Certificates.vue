90203ae9569882de6f5b574da6a34258d6ae07d8
joaoluis17 <jluispprado@hotmail.com>
2026-03-05 20:48:25 -0300

    removendo temporariamente botao dos certificados que nao funciona

--- src/views/Certificates.vue
+++ src/views/Certificates.vue
@@ -13,12 +13,12 @@
           >
             <h3 class="text-xl text-gray-600 font-bold mb-2">{{ certificate.title }}</h3>
             <p class="text-gray-600">{{ certificate.description }}</p>
-            <button
+            <!-- <button
               @click.stop="showcertificateDetails(index)"
               class="mt-4 bg-blue-400 text-white py-2 px-4 rounded hover:bg-blue-700"
             >
               Ver Detalhes
-            </button>
+            </button> -->
           </div>
         </div>
       </div>
