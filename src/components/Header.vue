774d349d779c9a83cb26e5917ed24ad9349e411e
joaoluis17 <jluispprado@hotmail.com>
2026-03-21 18:22:37 -0300

    atualizando a page de projetos e o botão de modo escuro

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -48,6 +48,17 @@
         <li>
           <router-link to="/contact" class="block py-2 px-3 hover:text-blue-400" @click="menuOpen = false">Contato</router-link>
         </li>
+
+        <li>
+          <button @click.stop="toggleDark(); menuOpen = false" class="block py-2 px-3 hover:text-blue-400">
+            <svg v-if="!isDark" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+            </svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+              <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clip-rule="evenodd" />
+            </svg>
+          </button>
+        </li>
       </ul>
 
     </nav>
@@ -55,11 +66,18 @@
 </template>
 
 <script>
+import { useDark, useToggle } from "@vueuse/core";
+
 export default {
   data() {
     return {
       menuOpen: false, 
     };
+  },
+  setup() {
+    const isDark = useDark();
+    const toggleDark = useToggle(isDark);
+    return { isDark, toggleDark };
   },
   methods: {
     goToSection(section) {
